804622d09e918d0c5539e33640786860698756da
wulj <wulj@rd.netease.com>
2013-01-28 14:41:40 +0800

    MOD：weinre 支持

--- src/build/generate/moduleTestPage.js
+++ src/build/generate/moduleTestPage.js
@@ -5,14 +5,18 @@
  * @version: 1.0
  */
 var fs = require('fs');
-var tp = require('../.');
+var tp = require('../../template');
 var path = require('path');
 var info = require('../../info');
 var config = require('./package.json');
+var os =  require('os');
 
 var output = path.resolve(__dirname, config.output),
     templatePage = path.resolve(__dirname, config.templatePage),
     suffix = config.templateSuffix;
+
+var hostPath = os.networkInterfaces()['以太网'][1].address;//本机ip
+
 /**
  * 使用模版中的css html模版，数据，初始化函数，生成测试文件
  * @param filedir 文件夹路径
@@ -31,12 +35,13 @@ exports.generate = function (filedir, value) {
                     renderTestPage({
                         title: value + '/' + key,
                         data: JSON.stringify(data[key]),
-                        moduleId: value
+                        moduleId: value,
+                        hostPath: hostPath
                     })
                 );
             }
         }
-        info.logt('Generate testPage [' + value + '/' + key + suffix + '] success.');
+        info.logt('generate testPage [' + value + '/' + key + suffix + '] success.');
     } catch (err) {
         info.error('fail to generate the testPage for module:' + value);
         info.error('for detail:' + err + '\r\n');
