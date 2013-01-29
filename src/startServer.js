3703e7a5120d91f2661d5657837a7cf0d97798d4
KnightWu <pufanyouzhu_hero@163.com>
2013-01-29 19:28:24 +0800

    MOD：自动启动weinre在10089

--- src/startServer.js
+++ src/startServer.js
@@ -1,7 +1,20 @@
+var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var app = express();
-var path = require('path');
 
+//run local web server
 app.use(express.static(path.resolve(__dirname, '../')));
-
 app.listen(9527);
+
+//run weinre at httpPort 10089
+var rootPath = path.resolve(__dirname, '../node_modules/weinre');
+var lib = path.join(rootPath, 'lib');
+process.argv.push('--httpPort');
+process.argv.push('10089');
+process.argv.push('--boundHost');
+process.argv.push('-all-');
+var node_modules = path.join(rootPath, 'node_modules');
+require(path.join(node_modules, 'coffee-script'));
+require(path.join(lib, '/cli')).run();
+
