960741c4c5658492cc5fbbe967adc8640264330c
KnightWu <wulijian722@gmail.com>
2013-12-05 18:52:21 +0800

    MOD: 合并index.html到index.js的render方法

--- test/project/nav/index.js
+++ test/project/nav/index.js
@@ -1,14 +1,10 @@
 define(function (require, exports, module) {
-    var $ = require('jquery');
-    var _ = require('underscore');  // 工具类
-    var _$ = require('jsonselect');  // 工具类
-    var utils = require('utils/utils');
-    var tpHelper = require('tpHelper'); // 用于模版模块生成的工具类
     module.exports = {
         init: function () {
 
         },
-        render: function () {
+        render: function (_data) {
+            return '';
         }
     };
 });
