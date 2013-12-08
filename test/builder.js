a277dc72dc1cd622c2e86933603f43b375d86930
KnightWu <wulijian722@gmail.com>
2013-12-08 20:49:49 +0800

    MOD: 将编译好的模板方法合并到模块的render方法上，并使用mocha的异步测试测试结果

--- test/builder.js
+++ test/builder.js
@@ -3,32 +3,16 @@ var path = require('path');
 var fs = require('fs');
 var builder = require('../lib/builder');
 var q = require('q');
-var generateOptions = {
-    "indent_start": 0,
-    "indent_level": 4,
-    "quote_keys": true,
-    "space_colon": true,
-    "ascii_only": false,
-    "inline_script": false,
-    "width": 80,
-    "max_line_len": 32000,
-    "beautify": true,
-    "source_map": null,
-    "bracketize": false,
-    "comments": true,
-    "semicolons": true
-};
 
 describe('module builder', function () {
     var modulePath = path.resolve(__dirname, './project/content');
-    beforeEach(function () {
-        var code = fs.readFileSync(modulePath + '/index-backup.js');
-        fs.writeFile(modulePath + '/index.js', code);
-    });
     describe('give the module name, generate a module:', function () {
-        it('after build, the render function will return a string:', function () {
-            builder.build(modulePath).then(function(){
-                require('./project/content').render({}).should.eql('<link rel="stylesheet" href="content.css"/>\r\n<div id="content">\r\n    <puzzle data-module="./mod1"></puzzle>\r\n    <puzzle data-module="./mod1"></puzzle>\r\n</div>');
+        it('after build, the render function will return a string:', function (done) {
+            builder.build(modulePath, __dirname).then(function(){
+                require('./__modules/project/content').render({a: 1, b: 3})
+                    .should.eql('<link rel="stylesheet" href="content.css"/>\r\n<div id="content">\r\n   ' +
+                        ' <puzzle data-module="./mod1"></puzzle>\r\n    <puzzle data-module="./mod1"></puzzle>\r\n</div>');
+                done();
             });
         });
     });
--- test/project/content/index.js
+++ test/project/content/index.js
@@ -1,18 +1,12 @@
-if (typeof define !== "function") {
-    var define = require("amdefine")(module);
-}
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     module.exports = {
-        "init": function() {
+        "init": function () {
             return "It's great!!";
         },
-        "render":         function anonymous(_data) {
-            var htmlCode = "";
-            with (_data || {}) {
-                htmlCode += '<link rel="stylesheet" href="content.css"/>\r\n<div id="content">\r\n    <puzzle data-module="./mod1"></puzzle>\r\n    <puzzle data-module="./mod1"></puzzle>\r\n</div>';
-            }
-            return htmlCode;
+        "render": function (data) {
+            return ___template___(data);
         }
     };
 });
