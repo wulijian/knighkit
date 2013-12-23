b402eec47bce4bbfc83c9871cbe510084b8f1944
KnightWu <wulijian722@gmail.com>
2013-12-23 16:55:29 +0800

    MOD: 1. build all 2. 因build all需要，build接口变化 3. walkfile 代码提交频率低，一次提交的内容太多了。。。

--- test/builder.js
+++ test/builder.js
@@ -4,15 +4,16 @@ chai.use(chaiAsPromised);
 chai.should();
 var path = require('path');
 var fs = require('fs');
-var builder = require('../lib/builder');
+var builder = require('../lib/builder').root(path.resolve(__dirname, './project'));
 var q = require('q');
 var dc = require('../lib/runtime/dataCache');
+require("consoleplusplus");
 
 describe('module builder', function () {
     describe('give the module name, generate a module:', function () {
         describe("the module without sub module:", function () {
             it('build simple module without sub:', function (done) {
-                builder.build('project/content/mod3', __dirname)
+                builder.build(path.resolve(__dirname, 'project/content/mod3'))
                     .then(function () {
                         return require('./__project/content/mod3').render({a: 1, b: 3});
                     })
@@ -29,8 +30,8 @@ describe('module builder', function () {
         describe("the module with two layer sub module:", function () {
             it('parse sub module:', function (done) {
                 q.all([
-                        builder.build('project/content/mod1', __dirname),
-                        builder.build('project/content/mod3', __dirname)
+                        builder.build(path.resolve(__dirname, 'project/content/mod1')),
+                        builder.build(path.resolve(__dirname, 'project/content/mod3'))
                     ])
                     .then(function () {
                         return require('./__project/content/mod1').render({a: 1, b: 3})
@@ -48,12 +49,12 @@ describe('module builder', function () {
             this.timeout(3000);
             it('parse sub module:', function (done) {
                 q.all([
-                        builder.build('project/content', __dirname),
-                        builder.build('project/content/mod1', __dirname),
-                        builder.build('project/content/mod2', __dirname),
-                        builder.build('project/content/mod2/mod21', __dirname),
-                        builder.build('project/content/mod2/mod22', __dirname),
-                        builder.build('project/content/mod3', __dirname)
+                        builder.build(path.resolve(__dirname, 'project/content')),
+                        builder.build(path.resolve(__dirname, 'project/content/mod1')),
+                        builder.build(path.resolve(__dirname, 'project/content/mod2')),
+                        builder.build(path.resolve(__dirname, 'project/content/mod2/mod21')),
+                        builder.build(path.resolve(__dirname, 'project/content/mod2/mod22')),
+                        builder.build(path.resolve(__dirname, 'project/content/mod3'))
                     ])
                     .then(function () {
                         return require('./__project/content').render({a: 1, b: 3});
@@ -83,8 +84,8 @@ describe('module builder', function () {
         describe("submodule in for loop:", function () {
             it('parse sub module:', function (done) {
                 q.all([
-                        builder.build('project/foot', __dirname),
-                        builder.build('project/foot/mod1', __dirname)
+                        builder.build(path.resolve(__dirname, 'project/foot')),
+                        builder.build(path.resolve(__dirname, 'project/foot/mod1'))
                     ])
                     .then(function () {
                         return require('./__project/foot').render({a: 1, b: 3})
@@ -104,8 +105,8 @@ describe('module builder', function () {
         describe("puzzle in jade:", function () {
             it('parse sub module:', function (done) {
                 q.all([
-                        builder.build('project/nav', __dirname),
-                        builder.build('project/nav/mod1', __dirname)
+                        builder.build(path.resolve(__dirname, 'project/nav')),
+                        builder.build(path.resolve(__dirname, 'project/nav/mod1'))
                     ])
                     .then(function () {
                         return require('./__project/nav').render({a: 1, b: 3})
@@ -116,17 +117,18 @@ describe('module builder', function () {
             });
         });
 
-        describe("异步模块添加dom到成功后的回调:", function () {
+//todo:怎样测试调用顺序
+        describe("异步模块添加dom到成功后按优先级回调:", function () {
             it('dom 回调:', function (done) {
                 q.all([
-                        builder.build('project/nav', __dirname),
-                        builder.build('project/nav/mod1/mod11', __dirname),
-                        builder.build('project/nav/mod1/mod12', __dirname),
-                        builder.build('project/nav/mod1', __dirname)
+                        builder.build(path.resolve(__dirname, 'project/nav')),
+                        builder.build(path.resolve(__dirname, 'project/nav/mod1/mod11')),
+                        builder.build(path.resolve(__dirname, 'project/nav/mod1/mod12')),
+                        builder.build(path.resolve(__dirname, 'project/nav/mod1'))
                     ])
                     .then(function () {
                         require('./__project/nav').render({a: 1, b: 3}).then(function (subModule) {
-                            subModule.async.promise.fail(function(err){
+                            subModule.async.promise.fail(function (err) {
                                 done(err);
                             });
                             subModule.async.resolve(done);
@@ -151,6 +153,20 @@ describe('module builder', function () {
             });
             it('模板自己请求数据，selector无作用:', function () {
             });
+        });
+
+        describe("遍历模块树:", function () {
+            it('遍历模块树:', function (done) {
+                require('../lib/builder/walkFile').walk(path.resolve(__dirname, './project'), false, function (value) {
+                    console.log(value);
+                });
+            })
+        });
+
+        describe.only("编译项目中所有的模块:", function () {
+            it('遍历模块树:', function (done) {
+                builder.buildAll();
+            })
         });
     });
 });
