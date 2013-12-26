9427bd1f2fb89a486a9f507d0b882edfe6758e9b
KnightWu <wulijian722@gmail.com>
2013-12-26 16:30:27 +0800

    MOD: 解决静态资源路径转换问题

--- test/builder.js
+++ test/builder.js
@@ -46,37 +46,37 @@ describe('module builder', function () {
         it('render module without sub module:', function (done) {
             require('./__project/content/mod3').render({a: 1, b: 3})
                 .get('html')
-                .should.eventually.eql('<link rel="stylesheet" href="mod3.css"/>\r\nmod3...').notify(done);
+                .should.eventually.match(new RegExp('<link rel="stylesheet" href=".*mod3.css"/>\r\nmod3...')).notify(done);
         });
 
         it('parse sub module with two layer sub module:', function (done) {
             require('./__project/content/mod1').render({a: 1, b: 3})
                 .get('html')
-                .should.eventually.eql('<link rel="stylesheet" href="mod1.css"/>\r\n' +
+                .should.eventually.match(new RegExp('<link rel="stylesheet" href=".*mod1.css"/>\r\n' +
                     'mod1...\r\n' +
-                    '<link rel="stylesheet" href="mod3.css"/>\r\n' +
+                    '<link rel="stylesheet" href=".*mod3.css"/>\r\n' +
                     'mod3...\r\n' +
-                    'mod1...').notify(done);
+                    'mod1...')).notify(done);
         });
 
         it('parse sub module with three layer sub module:', function (done) {
             require('./__project/content').render({a: 1, b: 3})
                 .get('html')
-                .should.eventually.match(new RegExp('<link rel=\"stylesheet\" href=\"content.css\"/>\r\n' +
+                .should.eventually.match(new RegExp('<link rel=\"stylesheet\" href=\".*content.css\"/>\r\n' +
                     '<div id=\"content\">\r\n    ' +
-                    '<link rel=\"stylesheet\" href=\"mod2.css\"/>\r\n' + // mod2 start
+                    '<link rel=\"stylesheet\" href=\".*mod2.css\"/>\r\n' + // mod2 start
                     'mod2....\r\n' +
                     '<div>\r\n' +
-                    '    <link rel=\"stylesheet\" href=\"mod21.css\"/>\r\n' +// mod21 start
+                    '    <link rel=\"stylesheet\" href=\".*mod21.css\"/>\r\n' +// mod21 start
                     '<div id="do"></div>\r\n' +
                     'mod21....\r\n' +// mod21 end
                     '</div>\r\n' +
-                    '<puzzle data-module="./mod22" data-async="true".*></puzzle>\r\n' +// mod22 should not be replaced
-                    '<link rel=\"stylesheet\" href=\"mod3.css\"/>\r\n' +  // mod3 start
+                    '<puzzle data-module=".*mod22" data-async="true".*></puzzle>\r\n' +// mod22 should not be replaced
+                    '<link rel=\"stylesheet\" href=\".*mod3.css\"/>\r\n' +  // mod3 start
                     'mod3...\r\nmod2....\r\n' +                           // mod3 end mod2 end
-                    '    <link rel=\"stylesheet\" href=\"mod1.css\"/>\r\n' + // mod1 start
+                    '    <link rel=\"stylesheet\" href=\".*mod1.css\"/>\r\n' + // mod1 start
                     'mod1...\r\n' +
-                    '<link rel=\"stylesheet\" href=\"mod3.css\"/>\r\n' +// mod3 start
+                    '<link rel=\"stylesheet\" href=\".*mod3.css\"/>\r\n' +// mod3 start
                     'mod3...\r\n' +// mod3 end
                     'mod1...\r\n' +
                     '</div>')).notify(done);// mod1 end
@@ -85,20 +85,20 @@ describe('module builder', function () {
         it('parse sub module with sub module in for loop:', function (done) {
             require('./__project/foot').render({a: 1, b: 3})
                 .get('html')
-                .should.eventually.eql('<link rel="stylesheet" href="index.css"/>\r\n' +
-                    '    <link rel="stylesheet" href="index.css"/>\r\n' +
+                .should.eventually.match(new RegExp('<link rel="stylesheet" href=".*index.css"/>\r\n' +
+                    '    <link rel="stylesheet" href=".*index.css"/>\r\n' +
                     'mod1 in /foot...\r\n' +
-                    '    <link rel="stylesheet" href="index.css"/>\r\n' +
+                    '    <link rel="stylesheet" href=".*index.css"/>\r\n' +
                     'mod1 in /foot...\r\n' +
-                    '    <link rel="stylesheet" href="index.css"/>\r\n' +
+                    '    <link rel="stylesheet" href=".*index.css"/>\r\n' +
                     'mod1 in /foot...\r\n' +
-                    'foot...').notify(done);
+                    'foot...')).notify(done);
         });
 
         it('parse sub module that puzzle in jade:', function (done) {
             require('./__project/nav').render({a: 1, b: 3})
                 .get('html')
-                .should.eventually.match(new RegExp('<link rel="stylesheet" href="nav.css"/>' +
+                .should.eventually.match(new RegExp('<link rel="stylesheet" href=".*nav.css"/>' +
                     '<puzzle data-module="./mod1" data-async="true".*></puzzle>')).notify(done);
         });
 
