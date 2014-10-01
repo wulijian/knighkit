13f294f3c3baa5b9cc20ecbcc7069d5da943f305
KnightWu <wulijian722@gmail.com>
2014-10-01 16:57:20 +0800

    MOD:  参数表示为属性时，不再使用臃肿的data-的形式，如果不指定属性值，默认为true。

--- test/findsub.js
+++ test/findsub.js
@@ -4,57 +4,50 @@ var fs = require('fs');
 var findSubModule = require('../lib/runtime/subModule/find');
 
 describe('遍历树形结构和查找子模块', function () {
-    describe('从 puzzle 标签中获取 data- 属性的值', function () {
-        it('只有 data- 属性', function () {
-            findSubModule.getDataFrom('<puzzle data-a="tes a" data-b="1" data-c="true"><\/puzzle>').should.eql({
+    describe('从 puzzle 标签中获取  属性的值', function () {
+        it('只有  属性', function () {
+            findSubModule.getDataFrom('a="tes a" b="1" c="true"').should.eql({
                 a: "tes a",
                 b: "1",
                 c: "true"
             });
         });
-        it('含有一个style属性', function () {
-            findSubModule.getDataFrom('<puzzle data-a="1" data-b="1" style="color:red" data-c="true" onclick=""><\/puzzle>').should.eql({
+        it('（无 =） + （有等号无值） 的属性', function () {
+            findSubModule.getDataFrom('a="1" b="" c d= "a" e = "b" f=4 h= 5 g =7').should.eql({
                 a: "1",
-                b: "1",
-                c: "true"
+                b: "",
+                c: "true",
+                "d": "a",
+                "e": "b",
+                "f": "4",
+                "g": "7",
+                "h": "5"
             });
         });
-        it('（无 =） + （有等号无值） 的data-属性', function () {
-            findSubModule.getDataFrom('<puzzle data-a="1" data-b="" data-c data-d=><\/puzzle>').should.eql({
-                a: "1",
-                b: ""
-            });
-        });
-        it('data-=类的属性', function () {
-            findSubModule.getDataFrom('<puzzle data-a="1"  data-=""  data-h="df" data-b="1" data-g= style="color:red" data-c="true"><\/puzzle>').should.eql({
-                a: "1",
-                b: "1",
-                h: "df",
-                c: "true"
+        it('=类的属性', function () {
+            findSubModule.getDataFrom('a="1" g= style="color:red"').should.eql({
+                a: '1',
+                g: '',
+                style: 'color:red'
             });
         });
         it('属性字符串是单引号', function () {
-            findSubModule.getDataFrom('<puzzle data-a=\'1\'></puzzle>').should.eql({
+            findSubModule.getDataFrom('a=\'1\'').should.eql({
                 a: "1"
             });
         });
         it('属性中含有单引号', function () {
-            findSubModule.getDataFrom('<puzzle data-a="\'1\'23"></puzzle>').should.eql({
+            findSubModule.getDataFrom('a="\'1\'23"').should.eql({
                 a: "\'1\'23"
             });
         });
         it('含有转义字符的字符串', function () {
-            findSubModule.getDataFrom('<puzzle data-a="\"1\"23"></puzzle>').should.eql({
+            findSubModule.getDataFrom('a="\"1\"23"').should.eql({
                 a: "\"1\"23"
             });
         });
-        it('属性值是数字', function () {
-            findSubModule.getDataFrom('<puzzle data-a=34></puzzle>').should.eql({
-                a: "34"
-            });
-        });
         it('属性值是布尔型', function () {
-            findSubModule.getDataFrom('<puzzle data-a=true></puzzle>').should.eql({
+            findSubModule.getDataFrom('a=true').should.eql({
                 a: "true"
             });
         });
@@ -67,26 +60,26 @@ describe('遍历树形结构和查找子模块', function () {
                 {
                     module: './content',
                     priority: "2",
-                    puz: '<puzzle data-module="./content" data-priority="2"></puzzle>'
+                    puz: '<puzzle module="./content" priority="2"></puzzle>'
                 },
                 {
                     module: './foot',
                     priority: "1",
                     new: "false",
                     async: "true",
-                    puz: '<puzzle data-module="./foot" data-priority="1" data-new="false" data-async="true"></puzzle>'
+                    puz: '<puzzle module="./foot" priority="1" async></puzzle>'
                 },
                 {
                     "module": "./bottom",
                     "priority": "1",
                     "new": "false",
                     "async": "true",
-                    "puz": "<puzzle data-module=\"./bottom\" data-priority=\"1\" data-new=\"false\" data-async=\"true\"></puzzle>"
+                    "puz": "<puzzle module=\"./bottom\" priority=\"1\" async></puzzle>"
                 },
                 {
                     module: './nav',
                     new: "true",
-                    puz: '<puzzle data-module="./nav" data-new="true"></puzzle>'
+                    puz: '<puzzle module="./nav" new></puzzle>'
                 }
             ]);
         });
